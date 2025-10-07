19595157281205d6892a080d3b91314a9e29a808
NikitaAnpilogov <nikanp8@gmail.com>
2025-10-07 21:19:41 +0300

    Этап 3: Реализовал комментарии

--- ewm-service/src/main/java/ru/practicum/model/Comment.java
+++ ewm-service/src/main/java/ru/practicum/model/Comment.java
@@ -3,9 +3,7 @@ package ru.practicum.model;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
 
